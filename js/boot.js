658d80b633b00a297e3e657031a9b6d199f99dac
Rafael Karlo Delos Santos <rkdelossantos@gmail.com>
2015-11-28 14:49:07 +1100

    Added template for a simple game

--- js/boot.js
+++ js/boot.js
@@ -1,3 +1,53 @@
-/**
- * Created by Rafael on 28/11/2015.
- */
+var Game = function (game) {
+};
+
+Game.prototype = {
+    preload: function () {
+        this.game.load.image('sky', 'assets/sky.png');
+        this.game.load.image('ground', 'assets/platform.png');
+        this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    },
+    create: function () {
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.add.sprite(0, 0, 'sky');
+
+        platforms = this.game.add.group();
+
+        platforms.enableBody = true;
+
+        var ground = platforms.create(0, this.game.world.height - 64, 'ground');
+        ground.scale.setTo(2, 2);
+        ground.body.immovable = true;
+
+        player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
+        this.game.physics.arcade.enable(player);
+
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 300;
+        player.body.collideWorldBounds = true;
+
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        cursors = this.game.input.keyboard.createCursorKeys();
+    },
+    update: function () {
+        this.game.physics.arcade.collide(player, platforms);
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown) {
+            player.body.velocity.x = -150;
+            player.animations.play('left');
+        } else if (cursors.right.isDown) {
+            player.body.velocity.x = 150;
+            player.animations.play('right');
+        } else {
+            player.animations.stop();
+            player.frame = 4;
+        }
+
+        if (cursors.up.isDown && player.body.touching.down) {
+            player.body.velocity.y = -350;
+        }
+    }
+}
