34b7458e03e0d89f71fc06d476efbf001764a7ec
Rafael Karlo Delos Santos <rkdelossantos@gmail.com>
2015-11-28 22:27:00 +1100

    Starting template to use

--- js/boot.js
+++ js/boot.js
@@ -4,50 +4,47 @@ var Game = function (game) {
 Game.prototype = {
     preload: function () {
         this.game.load.image('sky', 'assets/sky.png');
-        this.game.load.image('ground', 'assets/platform.png');
         this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     },
     create: function () {
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.add.sprite(0, 0, 'sky');
 
-        platforms = this.game.add.group();
+        this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
 
-        platforms.enableBody = true;
+        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-        var ground = platforms.create(0, this.game.world.height - 64, 'ground');
-        ground.scale.setTo(2, 2);
-        ground.body.immovable = true;
+        this.cursors = this.game.input.keyboard.createCursorKeys();
 
-        player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
-        this.game.physics.arcade.enable(player);
+        //Add a tile
+        this.tiles = this.game.add.group();
+        var tile = this.game.add.bitmapData(50, 50);
+        tile.alpha = 1;
+        tile.ctx.rect(5, 5, 45, 45);
+        tile.ctx.fillStyle = '#888888';
+        tile.ctx.globalAlpha = 0.2;
+        tile.ctx.fill();
 
-        player.body.bounce.y = 0.2;
-        player.body.gravity.y = 300;
-        player.body.collideWorldBounds = true;
+        tile.ctx.font = '30px Arial';
+        tile.ctx.textAlign = 'center';
+        tile.ctx.textBaseline = 'middle';
+        tile.ctx.fillStyle = '#ffffff';
+        tile.ctx.globalAlpha = 1;
+        tile.ctx.fillText('A', 25, 25);
+        this.tiles.create(0, 0, tile);
 
-        player.animations.add('left', [0, 1, 2, 3], 10, true);
-        player.animations.add('right', [5, 6, 7, 8], 10, true);
+        var leftKey = this.game.input.keyboard.addKey(Phaser.KeyCode.RIGHT);
+        leftKey.onDown.add(function () {
+            this.player.animations.play('right');
+            this.player.x = this.player.x + 10;
+            this.player.animations.stop();
+        }, this);
 
-        cursors = this.game.input.keyboard.createCursorKeys();
+        var rightKey = this.game.input.keyboard.addKey(Phaser.KeyCode.LEFT);
+        rightKey.onDown.add(function () {
+            this.player.animations.play('left');
+            this.player.x = this.player.x - 10;
+            this.player.animations.stop();
+        }, this);
     },
-    update: function () {
-        this.game.physics.arcade.collide(player, platforms);
-        player.body.velocity.x = 0;
-
-        if (cursors.left.isDown) {
-            player.body.velocity.x = -150;
-            player.animations.play('left');
-        } else if (cursors.right.isDown) {
-            player.body.velocity.x = 150;
-            player.animations.play('right');
-        } else {
-            player.animations.stop();
-            player.frame = 4;
-        }
-
-        if (cursors.up.isDown && player.body.touching.down) {
-            player.body.velocity.y = -350;
-        }
-    }
 }
--- js/tests/unit_tests.js
+++ js/tests/unit_tests.js
@@ -3,17 +3,8 @@
  * Created by Rafael on 27/11/2015.
  */
 
-//var GLOBAL_OBJECT = GLOBAL_OBJECT || {};
-var MY_NAMESPACE = MY_NAMESPACE || {};
-
-MY_NAMESPACE.something = 'hello'
-
-
-QUnit.test("Object Testing", function(assert){
-   assert.ok(MY_NAMESPACE, "object exists");
-
-   assert.equal(MY_NAMESPACE.something, "hello", "equal");
-
+QUnit.test("NinjaVim Instance", function(assert){
+   assert.ok(NinjaVim, "object exists");
 });
 
 
