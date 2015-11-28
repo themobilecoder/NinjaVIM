3a374e8f30966f33c86f4e1cc39d0775f1dcbc88
Rafael Karlo Delos Santos <rkdelossantos@gmail.com>
2015-11-28 23:52:21 +1100

    Implemented tile generator with movement of cursor

--- js/boot.js
+++ js/boot.js
@@ -2,49 +2,71 @@ var Game = function (game) {
 };
 
 Game.prototype = {
+    init: function() {
+        this.grid_rows = 20;
+        this.grid_columns = 20;
+        this.tile_width = this.game.world.width / this.grid_rows;
+        this.tile_height = this.game.world.height / this.grid_columns;
+    },
     preload: function () {
-        this.game.load.image('sky', 'assets/sky.png');
-        this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        this.game.load.image('dude', 'assets/star.png', 0, 0);
     },
     create: function () {
-        this.game.add.sprite(0, 0, 'sky');
+        this.game.stage.backgroundColor = '#CDCDCD';
 
-        this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
-
-        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-        this.cursors = this.game.input.keyboard.createCursorKeys();
-
-        //Add a tile
         this.tiles = this.game.add.group();
-        var tile = this.game.add.bitmapData(50, 50);
-        tile.alpha = 1;
-        tile.ctx.rect(5, 5, 45, 45);
-        tile.ctx.fillStyle = '#888888';
-        tile.ctx.globalAlpha = 0.2;
-        tile.ctx.fill();
+        this.createTiles()
 
-        tile.ctx.font = '30px Arial';
-        tile.ctx.textAlign = 'center';
-        tile.ctx.textBaseline = 'middle';
-        tile.ctx.fillStyle = '#ffffff';
-        tile.ctx.globalAlpha = 1;
-        tile.ctx.fillText('A', 25, 25);
-        this.tiles.create(0, 0, tile);
-
-        var leftKey = this.game.input.keyboard.addKey(Phaser.KeyCode.RIGHT);
-        leftKey.onDown.add(function () {
-            this.player.animations.play('right');
-            this.player.x = this.player.x + 10;
-            this.player.animations.stop();
-        }, this);
-
-        var rightKey = this.game.input.keyboard.addKey(Phaser.KeyCode.LEFT);
+        var rightKey = this.game.input.keyboard.addKey(Phaser.KeyCode.RIGHT);
         rightKey.onDown.add(function () {
-            this.player.animations.play('left');
-            this.player.x = this.player.x - 10;
-            this.player.animations.stop();
+            this.player.x = this.player.x + this.tile_width;
         }, this);
+
+        var leftKey = this.game.input.keyboard.addKey(Phaser.KeyCode.LEFT);
+        leftKey.onDown.add(function () {
+            this.player.x = this.player.x - this.tile_width;
+        }, this);
+
+        var upKey = this.game.input.keyboard.addKey(Phaser.KeyCode.UP);
+        upKey.onDown.add(function () {
+            this.player.y = this.player.y - this.tile_height;
+        }, this);
+
+        var downKey = this.game.input.keyboard.addKey(Phaser.KeyCode.DOWN);
+        downKey.onDown.add(function () {
+            this.player.y = this.player.y + this.tile_height;
+        }, this);
+
+        this.player = this.game.add.sprite(0,0,'dude');
+        this.player.alpha = 0.7;
+        this.player.width = this.tile_width;
+        this.player.height = this.tile_height;
+
     },
+
+    createTiles: function () {
+        for (var x = 0; x < this.grid_rows;  ++x) {
+            for (var y = 0; y < this.grid_columns; ++y) {
+                var tile = this.game.add.bitmapData(this.tile_width, this.tile_height);
+                tile.alpha = 1;
+                tile.ctx.rect(0, 0, this.tile_width, this.tile_height);
+                tile.ctx.fillStyle = '#CC0000';
+                tile.ctx.globalAlpha = 0.2;
+                tile.ctx.fill();
+
+                tile.ctx.font = '20px Arial';
+                tile.ctx.textAlign = 'center';
+                tile.ctx.textBaseline = 'middle';
+                tile.ctx.fillStyle = '#000000';
+                tile.ctx.globalAlpha = 1;
+                tile.ctx.fillText(this.getRandomLetter(), this.tile_width/2, this.tile_height/2);
+
+                this.tiles.create(x * this.tile_width, y * this.tile_height, tile);
+            }
+        }
+    },
+    getRandomLetter: function() {
+        return String.fromCharCode(97 + Math.floor(Math.random() * 26));
+
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var NinjaVim = NinjaVim || {};
 
-NinjaVim.game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+NinjaVim.game = new Phaser.Game(800, 800, Phaser.AUTO, '');
 
 NinjaVim.game.state.add('Game', Game);
 NinjaVim.game.state.start('Game');
