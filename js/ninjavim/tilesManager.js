69cad5fc878dfd4cb23ccc2adeb8ecf715f6400b
Rafael Karlo Delos Santos <rkdelossantos@gmail.com>
2015-12-03 22:31:39 +1100

    Set tile boundaries in setting a letter in TilesManager

--- js/ninjavim/tilesManager.js
+++ js/ninjavim/tilesManager.js
@@ -21,25 +21,46 @@ TilesManager.prototype = {
         }
     },
     setLetterToColumn: function (letter, column, row) {
+        column = this._normalizeInputColumn(column);
+        row = this._normalizeInputRow(row);
         var tile = this.builder.setLetter(letter).build();
-        var key = this.getKey(column, row);
+        var key = this._getKey(column, row);
         var currentTile = this.tiles[key];
         if (currentTile != undefined) currentTile.clear();
         this.tiles[key] = tile;
         return tile;
     },
     getLetterFromColumn: function (column, row) {
-        var key = this.getKey(column, row);
-        return this.tiles[key].letter;
+        var key = this._getKey(column, row);
+        var tileInColumnRow = this.tiles[key];
+        return tileInColumnRow != undefined ? tileInColumnRow.letter : '';
     },
     draw: function (tile, column, row) {
         this.tilesContainer.create(column * this.tileWidth, row * this.tileHeight, tile);
     },
-    getKey: function (column, row) {
+    _getKey: function (column, row) {
         if (parseInt(column) >= 0 && parseInt(column) < 100 && parseInt(row) >= 0 && parseInt(column) < 100) {
             if(column < 10) {column = '0' + column.toString()} else {column = column.toString();}
             if(row < 0) {row = '0' + column.toString()} else {row = row.toString();}
             return column + row;
+        }
+    },
+    _normalizeInputColumn: function(column) {
+        if (column >= this.columns) {
+            return this.columns - 1;
+        } else if (column < 0) {
+            return 0;
+        } else {
+            return column;
+        }
+    },
+    _normalizeInputRow: function(row) {
+        if (row >= this.rows) {
+            return this.rows - 1;
+        } else if (row < 0) {
+            return 0;
+        } else {
+            return row;
         }
     }
 };
--- js/tests/tiles_manager_test.js
+++ js/tests/tiles_manager_test.js
@@ -12,10 +12,43 @@ var builderStub = {
         return this;
     }
 };
+var maxColumns = 20;
+var maxRows = 20;
+
 QUnit.test("TilesManager should update letters in tiles", function(assert){
-    assert.ok(TilesManager, "TilesManager is defined");
-    var tilesManager = new TilesManager(gameStub, 20, 20, {}, builderStub);
-    assert.equal(tilesManager.tiles[tilesManager.getKey(0,0)], undefined, "Tiles should undefined" );
+    var tilesManager = new TilesManager(gameStub, maxColumns, maxRows, {}, builderStub);
+    assert.equal(tilesManager.tiles[tilesManager._getKey(0,0)], undefined, "Tiles should undefined" );
     tilesManager.setLetterToColumn('A',0,0);
     assert.equal(tilesManager.getLetterFromColumn(0,0), 'A', "Letter should have been set");
+    tilesManager.setLetterToColumn('C',19,19);
+    assert.equal(tilesManager.getLetterFromColumn(19,19), 'C', "Letter should have been set");
+});
+
+QUnit.test("TilesManager should prevent set letter at the edge when input is outside the boundary", function(assert) {
+    var tilesManager = new TilesManager(gameStub, maxColumns, maxRows, {}, builderStub);
+
+    var oneColumnPastTheBoundary = 20;
+    tilesManager.setLetterToColumn('L', oneColumnPastTheBoundary, 0);
+    assert.equal(tilesManager.getLetterFromColumn(oneColumnPastTheBoundary, 0), '', 'No letter should be set past the column boundary');
+
+    var rightMostColumn = 19;
+    assert.equal(tilesManager.getLetterFromColumn(rightMostColumn, 0), 'L', 'Letter should be set at the boundary');
+
+    var oneRowPastTheBoundary = 20;
+    tilesManager.setLetterToColumn('M', 0, oneRowPastTheBoundary);
+    assert.equal(tilesManager.getLetterFromColumn(0, oneRowPastTheBoundary), '', 'No letter should be set past the column boundary');
+
+    var lowestRow = 19;
+    assert.equal(tilesManager.getLetterFromColumn(0, lowestRow), 'M', 'Letter should be set at the boundary');
+
+    var oneColumnLeftOfBoundary = -1;
+    var oneRowAboveBoundary = -1;
+    tilesManager.setLetterToColumn('N', oneColumnLeftOfBoundary, oneRowAboveBoundary);
+    assert.equal(tilesManager.getLetterFromColumn(oneColumnLeftOfBoundary, oneRowAboveBoundary), '', 'No letter should be set past the column boundary');
+
+    var upMostRow = 0;
+    var leftMostColumn = 0;
+    assert.equal(tilesManager.getLetterFromColumn(leftMostColumn, upMostRow), 'N', 'Letter should be set at the boundary');
+
+
 });
